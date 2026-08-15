08405ed3c000c26e0904f5dc9dfb5db566c99694
salamsrca89 <mohalahmdi.m@gmail.com>
2026-08-15 03:33:23 +0300

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -1,13 +1,1 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema";
 
-// قراءة رابط الاتصال من متغيرات البيئة في Render
-const connectionString = process.env.DATABASE_URL!;
-
-// إعداد عميل الاتصال بقاعدة بيانات PostgreSQL الخارجية
-const client = postgres(connectionString, { ssl: 'require' });
-
-export function getDb() {
-  return drizzle(client, { schema });
-}
